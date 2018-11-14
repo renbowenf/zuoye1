1f6926b759341b689de076fafe0fa61805dd3fa7
renbowenf <45034854+renbowenf@users.noreply.github.com>
2018-11-14 22:29:20 +0800

    Update elegance.php

--- elegance.php
+++ elegance.php
@@ -1,13 +1,14 @@
-<?php
+<?php   
+    function a(){ 
     $b=0;
-    function a($b){ 
-}
     for($x=-5;$x<=5;$x++){
         for($y=-5;$y<=5;$y++){
-            if(($x*$x)+($y*$y)==25){ 
-            $b=$b++;            
-        }
-    }
+            if($x*$x+$y*$y==25){ 
+            $b=$b+1;            
+         }
+      }
+   }
+print_r("$b");
 }
-print a($b);
+a();
 ?>
